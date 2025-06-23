875f9a96508ccb269923128299858fe3de026b3b
Viktoriia <v.makovskaya356@gmail.com>
2025-06-24 01:04:11 +0300

    updatevite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import path from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
@@ -14,26 +14,19 @@ export default defineConfig(({ command }) => {
     build: {
       sourcemap: true,
       rollupOptions: {
-        // Змінено на пошук html-файлів відносно root
-        input: glob.sync('*.html'),
+        input: {
+          main: path.resolve(__dirname, 'src/index.html'),
+          gallery: path.resolve(__dirname, 'src/1-gallery.html'),
+          form: path.resolve(__dirname, 'src/2-form.html'),
+        },
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+          entryFileNames: '[name].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
         },
       },
       outDir: '../dist',
@@ -41,11 +34,8 @@ export default defineConfig(({ command }) => {
     },
     plugins: [
       injectHTML(),
-      // Шлях змінився, щоб коректно спрацював з root='src'
       FullReload(['**/*.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
+      SortCss({ sort: 'mobile-first' }),
     ],
   };
 });
